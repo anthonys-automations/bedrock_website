cf78571589693d79c78d68186c037a21bd85aa24
Anthony Verevkin <anthony@verevkin.ca>
2025-04-11 18:38:53 -0400

    add proxy

--- bedrock/config/environments/staging.php
+++ bedrock/config/environments/staging.php
@@ -21,3 +21,6 @@ Config::define('WP_MEMORY_LIMIT', '128M');
 Config::define('WP_DEBUG', true);
 Config::define('WP_DEBUG_DISPLAY', true);
 Config::define('WP_DEBUG_LOG', '/var/log/php/wp.log');
+
+define('WP_PROXY_HOST', 'infra2.oci.verevkin.ca');
+define('WP_PROXY_PORT', '8888');

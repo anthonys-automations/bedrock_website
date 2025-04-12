9bf73a19a46c1c78b429cb1f37c06a66f4997cc1
Anthony Verevkin <anthony@verevkin.ca>
2025-04-12 10:03:22 -0400

    disable sidebar and minor css change

--- bedrock/web/app/themes/treasure-chest/sidebar.php
+++ bedrock/web/app/themes/treasure-chest/sidebar.php
@@ -1,7 +1,9 @@
 <?php
 // sidebar.php
+/*
 if ( is_active_sidebar( 'main-sidebar' ) ) : ?>
   <aside id="secondary" class="sidebar widget-area">
     <?php dynamic_sidebar( 'main-sidebar' ); ?>
   </aside>
+*/
 <?php endif; ?>

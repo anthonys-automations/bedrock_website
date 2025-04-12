0963b35e9456a097f07e4c456e4de58a915d47a7
Anthony Verevkin <anthony@verevkin.ca>
2025-04-12 10:17:51 -0400

    fix sidebar

--- bedrock/web/app/themes/treasure-chest/sidebar.php
+++ bedrock/web/app/themes/treasure-chest/sidebar.php
@@ -1,9 +1,9 @@
 <?php
 // sidebar.php
-/*
 if ( is_active_sidebar( 'main-sidebar' ) ) : ?>
+  /*
   <aside id="secondary" class="sidebar widget-area">
-    <?php dynamic_sidebar( 'main-sidebar' ); ?>
+    <?php /* dynamic_sidebar( 'main-sidebar' ); */ ?>
   </aside>
 */
 <?php endif; ?>

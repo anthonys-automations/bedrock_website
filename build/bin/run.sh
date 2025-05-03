5a3af4b4a081819e216562b7956f449425832a30
Anthony Verevkin <anthony@verevkin.ca>
2025-05-02 21:02:23 -0400

    analytics permissions

--- build/bin/run.sh
+++ build/bin/run.sh
@@ -3,6 +3,8 @@
 chown www-data:www-data /srv/bedrock/web/app/
 chown www-data:www-data /srv/bedrock/web/app/uploads/
 #chown www-data:www-data /srv/bedrock/web/app/plugins/
+mkdir -p /srv/bedrock/web/app/plugins/independent-analytics/temp/
+chown www-data:www-data /srv/bedrock/web/app/plugins/independent-analytics/temp/
 
 openssl req -x509 -newkey rsa:4096 -keyout /etc/nginx/server.key -out /etc/nginx/server.crt -sha256 -days 3650 -nodes -subj "/CN=${SITE_NAME}"
 cp /etc/nginx/server.crt /usr/local/share/ca-certificates/ && update-ca-certificates
